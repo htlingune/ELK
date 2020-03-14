cf7ed4054abe33ab70b72ea1733c5a967cfcff23
htlingune <59502548+htlingune@users.noreply.github.com>
2020-03-14 16:14:07 +0800

    kafka

--- consumer.py
+++ consumer.py
@@ -16,12 +16,21 @@ def my_assign(consumer_instance, partitions):
     print('assign', partitions)
     consumer_instance.assign(partitions)
 
+def print_commit_result(err, partitions):
+    if err:
+        print('# Failed to commit offsets: %s: %s' % (err, partitions))
+    else:
+        for p in partitions:
+            print('# Committed offsets for: %s-%s {offset=%s}' % (p.topic, p.partition, p.offset))
+
 if __name__ == '__main__':
     props = {
         'bootstrap.servers': 'localhost:9092',
-        'group.id': 'STUDENT_ID',
+        'group.id': 'stock_tracker',
         'auto.offset.reset': 'earliest',
-        'session.timeout.ms': 6000,
+        'enable.auto.commit': True,
+        'auto.commit.interval.ms': 6000,
+        'on_commit': print_commit_result,
         'error_cb': error_cb
     }
     consumer = Consumer(props)
@@ -30,7 +39,7 @@ if __name__ == '__main__':
     count = 0
     try:
         while True:
-            records = consumer.consume(num_messages=1, timeout=1.0)
+            records = consumer.consume(num_messages=3, timeout=1.0)
             if records is None:
                 continue
             for record in records:
@@ -41,24 +50,18 @@ if __name__ == '__main__':
                         sys.stderr.write('%% {} [{}] reached end at offset {} - {}\n'.format(record.topic(), record.partition(), record.offset()))
                     else:
                         raise KafkaException(record.error())
+                else:
+                    topic = record.topic()
+                    partition = record.partition()
+                    offset = record.offset()
+                    timestamp = record.timestamp()
+                    msgKey = try_decode_utf8(record.key())
+                    msgValue = try_decode_utf8(record.value())
+                    print('%s-%d-%d : (%s , %s)' % (topic, partition, offset, msgKey, msgValue))
+    except KeyboardInterrupt as e:
+        sys.stderr.write('Aborted by user\n')
+    except Exception as e:
+        sys.stderr.write(e)
+    finally:
+        consumer.close()
 
-
-
-
-
-'股票名稱' = content['msgArray'][0]['n']
-'時間' = content['msgArray'][0]['t']
-'昨日收盤價' = content['msgArray'][0]['y']
-'開盤價' = content['msgArray'][0]['o']
-'最高價' = content['msgArray'][0]['h']
-'最低價' = content['msgArray'][0]['l']
-'上市櫃' = content['msgArray'][0]['ex']
-'最近成交價' = content['msgArray'][0]['z']
-'最新交易成交張數' = content['msgArray'][0]['tv']
-'當日成交量' = content['msgArray'][0]['v']
-'五檔報價賣出價' = [i for i in content['msgArray'][0]['a'].split('_')][0:-1]
-'五檔報價賣出量' = [i for i in content['msgArray'][0]['f'].split('_')][0:-1]
-'五檔報價買入價' = [i for i in content['msgArray'][0]['b'].split('_')][0:-1]
-'五檔報價買入量' = [i for i in content['msgArray'][0]['g'].split('_')][0:-1]
-'漲停點' = content['msgArray'][0]['u']
-'跌停點' = content['msgArray'][0]['w']
--- producer.py
+++ producer.py
@@ -4,15 +4,31 @@ import time
 import requests
 import json
 
-def get_liveprice():
+def get_liveprice(no):
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'}
-    stocknumber = 2330
+    stocknumber = no
     query = 'http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_{}.tw'.format(stocknumber)
     session = requests.session()
-    response = requests.get(query, headers=headers)
+    response = session.get(query, headers=headers)
     content = json.loads(response.text)
-    return content
+    股票名稱 = content['msgArray'][0]['n']
+    時間 = content['msgArray'][0]['t']
+    昨日收盤價 = content['msgArray'][0]['y']
+    開盤價 = content['msgArray'][0]['o']
+    最高價 = content['msgArray'][0]['h']
+    最低價 = content['msgArray'][0]['l']
+    上市櫃 = content['msgArray'][0]['ex']
+    最近成交價 = content['msgArray'][0]['z']
+    最新交易成交張數 = content['msgArray'][0]['tv']
+    當日成交量 = content['msgArray'][0]['v']
+    五檔報價賣出價 = [i for i in content['msgArray'][0]['a'].split('_')][0:-1]
+    五檔報價賣出量 = [i for i in content['msgArray'][0]['f'].split('_')][0:-1]
+    五檔報價買入價 = [i for i in content['msgArray'][0]['b'].split('_')][0:-1]
+    五檔報價買入量 = [i for i in content['msgArray'][0]['g'].split('_')][0:-1]
+    漲停點 = content['msgArray'][0]['u']
+    跌停點 = content['msgArray'][0]['w']
+    return 股票名稱, 時間, 昨日收盤價, 開盤價, 最高價, 最低價
 
 def error_cb(err):
     print('Error: %s' % err)
@@ -28,20 +44,21 @@ if __name__ == '__main__':
     props = {
         'bootstrap.servers': 'localhost:9092',
         'error_cb': error_cb,
-        'retries' : '3'
+        'retries' : '3',
         'max.in.flight.requests.per.connection' : '1'
     }
     producer = Producer(props)
     topicName = 'stock'
     try:
-        content = get_liveprice()
-        content = json.dumps(content.__dict__)
+        no = '2330'
+        股票名稱, 時間, 昨日收盤價, 開盤價, 最高價, 最低價 = get_liveprice(str(no))
+        content = f'股票名稱:{股票名稱}, 時間:{時間}, 昨日收盤價:{昨日收盤價}, 開盤價:{開盤價}, 最高價:{最高價}, 最低價:{最低價}'
         while True:
-            producer.produce(topicName, key=2330, value = content,callback=delivery_callback)
+            producer.produce(topicName, key=no, value=content,callback=delivery_callback)
             producer.poll(0)
-            print("good", end="\r")
-            time.sleep(3)
-        print('Send '  + ' messages to Kafka')
+            print("messages have received by broker", end="\r")
+            time.sleep(4)
+        print(f'Send {no}'  + ' messages to Kafka')
     except BufferError as e:
         sys.stderr.write('%% Local producer queue is full ({} messages awaiting delivery): try again\n'
                          .format(len(producer)))
